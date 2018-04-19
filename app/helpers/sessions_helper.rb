56fbde4ba4cb8ae5f8b89fce41876f67f7ab432b
emi <sakahiga@yahoo.co.jp>
2018-04-19 11:34:56 +0000

    login

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,9 @@
+module SessionsHelper
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])   #@current_userがnilだったら、右辺が実行される
+  end
+
+  def logged_in?
+    !!current_user  #!!→否定の否定、必ずtrueかfalseが返る
+  end
+end
