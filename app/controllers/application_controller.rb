f839fe38fc133d06ea1bc6a20b63865deb7d96ec
emi <sakahiga@yahoo.co.jp>
2018-04-22 08:19:22 +0000

    want, have

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,5 +15,19 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])   #@current_userがnilだったら、右辺が実行される
   end    
-  
+
+
+  def read(result)
+    code = result["itemCode"]
+    name = result["itemName"]
+    url = result["itemUrl"]
+    image_url = result["mediumImageUrls"].first["imageUrl"].gsub("?_ex=128x128","")
+    
+    return{
+      code: code,
+      name: name,
+      url: url,
+      image_url: image_url,
+    }
+  end  
 end
